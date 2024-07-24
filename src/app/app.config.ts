c75f2ebcdc6831cb6a33f9bc8c4339db7d069362
Celil Ozturk <celil.ozturk@windowslive.com>
2024-07-24 23:45:24 +0200

    Environment, baseUrl injection

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,11 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { environment } from '../environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(),
+    {provide:"Base_Api_Url", useValue:environment.baseUrl}
+  ]
 };
--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  constructor(@Inject("Base_Api_Url") private baseUrl:string){
+    // this.baseUrlString=baseUrl;
+  }
 
 }
